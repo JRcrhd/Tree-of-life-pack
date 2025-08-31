235bbab6ef82ea2a3fb4cdbea43c9e2b26aa5b57
JRcrhd <116958216+JRcrhd@users.noreply.github.com>
2025-08-31 19:04:05 +0800

    update

--- kubejs/startup_scripts/CustomAdditions.js
+++ kubejs/startup_scripts/CustomAdditions.js
@@ -1,176 +1,190 @@
 const $BuiltInRegistries = Java.loadClass("net.minecraft.core.registries.BuiltInRegistries")
 
 StartupEvents.registry('block', treeoflife => { 
-    treeoflife.create('empty_dragon_egg').displayName('空龙蛋').requiresTool(true).stoneSoundType().model('kubejs:block/empty_dragon_egg');
+    treeoflife.create('empty_dragon_egg').requiresTool(true).stoneSoundType().model('kubejs:block/empty_dragon_egg');
 });
 
 StartupEvents.registry('item', treeoflife => {
-    treeoflife.create('point_malkuth').displayName('王国——Malkuth').glow(true).texture('kubejs:item/point/malkuth').tag('treeoflife:point');
-    treeoflife.create('point_yesod').displayName('基础——Yesod').glow(true).texture('kubejs:item/point/yesod').tag('treeoflife:point');
-    treeoflife.create('point_netzach').displayName('永恒——Netzach').glow(true).texture('kubejs:item/point/netzach').tag('treeoflife:point');
-    treeoflife.create('point_hod').displayName('荣耀——Hod').glow(true).texture('kubejs:item/point/hod').tag('treeoflife:point');
-    treeoflife.create('point_tiphareth').displayName('美丽——Tiphareth').glow(true).texture('kubejs:item/point/tiphareth').tag('treeoflife:point');
-    treeoflife.create('point_chesed').displayName('慈悲——Chesed').glow(true).texture('kubejs:item/point/chesed').tag('treeoflife:point');
-    treeoflife.create('point_geburah').displayName('严厉——Geburah').glow(true).texture('kubejs:item/point/geburah').tag('treeoflife:point');
-    treeoflife.create('point_chokmah').displayName('智慧——Chokmah').glow(true).texture('kubejs:item/point/chokmah').tag('treeoflife:point');
-    treeoflife.create('point_binah').displayName('理解——Binah').glow(true).texture('kubejs:item/point/binah').tag('treeoflife:point');
-    treeoflife.create('point_kether').displayName('王冠——Kether').glow(true).texture('kubejs:item/point/kether').tag('treeoflife:point');
+    treeoflife.create('point_malkuth').glow(true).texture('kubejs:item/point/malkuth').tag('treeoflife:point');
+    treeoflife.create('point_yesod').glow(true).texture('kubejs:item/point/yesod').tag('treeoflife:point');
+    treeoflife.create('point_netzach').glow(true).texture('kubejs:item/point/netzach').tag('treeoflife:point');
+    treeoflife.create('point_hod').glow(true).texture('kubejs:item/point/hod').tag('treeoflife:point');
+    treeoflife.create('point_tiphareth').glow(true).texture('kubejs:item/point/tiphareth').tag('treeoflife:point');
+    treeoflife.create('point_chesed').glow(true).texture('kubejs:item/point/chesed').tag('treeoflife:point');
+    treeoflife.create('point_geburah').glow(true).texture('kubejs:item/point/geburah').tag('treeoflife:point');
+    treeoflife.create('point_chokmah').glow(true).texture('kubejs:item/point/chokmah').tag('treeoflife:point');
+    treeoflife.create('point_binah').glow(true).texture('kubejs:item/point/binah').tag('treeoflife:point');
+    treeoflife.create('point_kether').glow(true).texture('kubejs:item/point/kether').tag('treeoflife:point');
 	
-	treeoflife.create('template_assiah').displayName('Assiah晋升模板').texture('kubejs:item/point/assiah_template').tag('treeoflife:template');
-	treeoflife.create('template_yetzirah').displayName('Yetzirah晋升模板').texture('kubejs:item/point/yetzirah_template').tag('treeoflife:template');
-	treeoflife.create('template_beriah').displayName('Beriah晋升模板').texture('kubejs:item/point/beriah_template').tag('treeoflife:template');
-	treeoflife.create('template_atziluth').displayName('Atziluth晋升模板').texture('kubejs:item/point/atziluth_template').tag('treeoflife:template');
+	treeoflife.create('template_assiah').texture('kubejs:item/point/assiah_template').tag('treeoflife:template');
+	treeoflife.create('template_yetzirah').texture('kubejs:item/point/yetzirah_template').tag('treeoflife:template');
+	treeoflife.create('template_beriah').texture('kubejs:item/point/beriah_template').tag('treeoflife:template');
+	treeoflife.create('template_atziluth').texture('kubejs:item/point/atziluth_template').tag('treeoflife:template');
 	
-	treeoflife.create('dimension_alloy_overworld').displayName('主世界维度合金').texture('kubejs:item/point/dimension_alloy_overworld').tag('treeoflife:dimension_alloy').tag('forge:ingots');
-	treeoflife.create('dimension_alloy_nether').displayName('下界维度合金').texture('kubejs:item/point/dimension_alloy_nether').tag('treeoflife:dimension_alloy').tag('forge:ingots');
-	treeoflife.create('dimension_alloy_the_end').displayName('末地维度合金').texture('kubejs:item/point/dimension_alloy_the_end').tag('treeoflife:dimension_alloy').tag('forge:ingots');
-	treeoflife.create('dimension_alloy_aether').displayName('以太维度合金').texture('kubejs:item/point/dimension_alloy_aether').tag('treeoflife:dimension_alloy').tag('forge:ingots');
+	treeoflife.create('dimension_alloy_overworld').texture('kubejs:item/point/dimension_alloy_overworld').tag('treeoflife:dimension_alloy').tag('forge:ingots');
+	treeoflife.create('dimension_alloy_nether').texture('kubejs:item/point/dimension_alloy_nether').tag('treeoflife:dimension_alloy').tag('forge:ingots');
+	treeoflife.create('dimension_alloy_the_end').texture('kubejs:item/point/dimension_alloy_the_end').tag('treeoflife:dimension_alloy').tag('forge:ingots');
+	treeoflife.create('dimension_alloy_aether').texture('kubejs:item/point/dimension_alloy_aether').tag('treeoflife:dimension_alloy').tag('forge:ingots');
 
-	treeoflife.create('alchemy').displayName('炼金').texture('kubejs:item/alchemy/icon');
-	treeoflife.create('clairvoyance').displayName('通灵').texture('kubejs:item/clairvoyance/icon');
-	treeoflife.create('divination').displayName('占卜').texture('kubejs:item/divination/icon');
+	treeoflife.create('alchemy').texture('kubejs:item/alchemy/icon');
+	treeoflife.create('clairvoyance').texture('kubejs:item/clairvoyance/icon');
+	treeoflife.create('divination').texture('kubejs:item/divination/icon');
 
-	treeoflife.create('emerald_tablet').displayName('§2§l《翠玉录》§r').glow(true).texture('kubejs:item/emerald_tablet');
-    treeoflife.create('emerald_tablet_piece_1').displayName('§2石板碎片—1§r').texture('kubejs:item/emerald_tablet_piece_1');
-    treeoflife.create('emerald_tablet_piece_2').displayName('§2石板碎片—2§r').texture('kubejs:item/emerald_tablet_piece_2');
-    treeoflife.create('emerald_tablet_piece_3').displayName('§2石板碎片—3§r').texture('kubejs:item/emerald_tablet_piece_3');
-    treeoflife.create('empty_tarot').displayName('空塔罗牌').texture('kubejs:item/divination/empty_tarot');
+	treeoflife.create('emerald_tablet').glow(true).texture('kubejs:item/emerald_tablet');
+    treeoflife.create('emerald_tablet_piece_1').texture('kubejs:item/emerald_tablet_piece_1');
+    treeoflife.create('emerald_tablet_piece_2').texture('kubejs:item/emerald_tablet_piece_2');
+    treeoflife.create('emerald_tablet_piece_3').texture('kubejs:item/emerald_tablet_piece_3');
+    treeoflife.create('empty_tarot').texture('kubejs:item/divination/empty_tarot');
 
-    treeoflife.create('alchemy_black').displayName('黑化').glow(true).texture('kubejs:item/alchemy/black');
-    treeoflife.create('alchemy_white').displayName('白化').glow(true).texture('kubejs:item/alchemy/white');
-    treeoflife.create('alchemy_yellow').displayName('黄化').glow(true).texture('kubejs:item/alchemy/yellow');
-    treeoflife.create('alchemy_red').displayName('赤化').glow(true).texture('kubejs:item/alchemy/red');
-    treeoflife.create('alchemy_in_phi').displayName('未完工的贤者之石').glow(true).texture('kubejs:item/alchemy/in_phi');
+    treeoflife.create('alchemy_black').glow(true).texture('kubejs:item/alchemy/black');
+    treeoflife.create('alchemy_white').glow(true).texture('kubejs:item/alchemy/white');
+    treeoflife.create('alchemy_yellow').glow(true).texture('kubejs:item/alchemy/yellow');
+    treeoflife.create('alchemy_red').glow(true).texture('kubejs:item/alchemy/red');
+    treeoflife.create('alchemy_in_phi').glow(true).texture('kubejs:item/alchemy/in_phi');
 
-    treeoflife.create('earth_alloy_shard').displayName('大地合金残片').texture('kubejs:item/tech/earth_alloy');
-    treeoflife.create('ember_alloy_shard').displayName('余烬合金残片').texture('kubejs:item/tech/ember_alloy');
-    treeoflife.create('void_alloy_shard').displayName('虚空合金残片').texture('kubejs:item/tech/void_alloy');
-    treeoflife.create('sun_alloy_shard').displayName('光芒合金残片').texture('kubejs:item/tech/sun_alloy');
+    treeoflife.create('earth_alloy_shard').texture('kubejs:item/tech/earth_alloy');
+    treeoflife.create('ember_alloy_shard').texture('kubejs:item/tech/ember_alloy');
+    treeoflife.create('void_alloy_shard').texture('kubejs:item/tech/void_alloy');
+    treeoflife.create('sun_alloy_shard').texture('kubejs:item/tech/sun_alloy');
 
-    treeoflife.create('magical_polonium').displayName('注魔钋').glow(true).texture('mekanism:item/pellet_polonium');
+    treeoflife.create('magical_polonium').glow(true).texture('mekanism:item/pellet_polonium');
+
+    treeoflife.create('holy_potion').texture('kubejs:item/holy_potion').maxStackSize(1)
+        .rarity('epic')
+        .useAnimation('drink')
+        .use((level, player, hand) => {
+            return true;
+        })
+        .useDuration(itemStack => 20)
+        .finishUsing((itemstack, level, entity) => {
+            if (level.isClientSide()) return itemstack
+            entity.server.runCommandSilent(`/lichdom revoke ${entity.name.getString()}`)
+            return;
+        })
+
 });
 
 StartupEvents.registry('fluid', treeoflife => {
     treeoflife.create('sky_fluid')
         .thinTexture(0x67E0D4)
         .bucketColor(0x67E0D4)
-        .displayName('天空流体')
+
         .temperature(280);
 
     treeoflife.create('witch_fluid')
         .thickTexture(0x090136)
         .bucketColor(0x090136)
-        .displayName('巫术流体')
+
         .temperature(330);
 
     treeoflife.create('dim_sky_fluid')
         .thinTexture(0x3087C2)
         .bucketColor(0x3087C2)
-        .displayName('暗淡的天空流体')
+
         .temperature(290);
 
     treeoflife.create('current_fluid')
         .thickTexture(0x072F4A)
         .bucketColor(0x072F4A)
-        .displayName('微缩洋流')
+
         .temperature(300);
 
     treeoflife.create('sunlight_fluid')
         .thinTexture(0xFFF838)
         .bucketColor(0xFFF838)
-        .displayName('日光')
+
         .temperature(320);
     
     treeoflife.create('molten_obsidian')
         .thickTexture(0x311140)
         .bucketColor(0x311140)
-        .displayName('熔融黑曜石')
+
         .temperature(2000);
 
     treeoflife.create('molten_dark_steel')
         .thickTexture(0x000E2B)
         .bucketColor(0x000E2B)
-        .displayName('熔融玄钢')
+
         .temperature(1200);
 
     treeoflife.create('dark_light_fluid')
         .thinTexture(0x981232)
         .bucketColor(0x981232)
-        .displayName('暗光')
+
         .temperature(200);
 
     treeoflife.create('void_fluid')
         .thinTexture(0x561232)
         .bucketColor(0x561232)
-        .displayName('虚空流体')
+
         .temperature(1);
 
     treeoflife.create('chaos_fluid')
         .thickTexture(0x941012)
         .bucketColor(0x941012)
-        .displayName('混沌流体')
+
         .temperature(1500);
 
     treeoflife.create('molten_end_steel')
         .thickTexture(0xF0E68C)
         .bucketColor(0xF0E68C)
-        .displayName('熔融末地钢')
+
         .temperature(3000);
 
     treeoflife.create('aether_fluid')
         .thinTexture(0xAAAAAA)
         .bucketColor(0xAAAAAA)
-        .displayName('以太流体')
+
         .temperature(0.1);
 
     treeoflife.create('root_fluid')
         .thinTexture(0xFFFFFF)
         .bucketColor(0xFFFFFF)
-        .displayName('根源流体')
+
         .temperature(10000);
 
     treeoflife.create('clear_water_tier_1')
         .thinTexture(0x4C46FF)
         .bucketColor(0x4C46FF)
-        .displayName('1级净化水')
+
 
     treeoflife.create('clear_water_tier_2')
         .thinTexture(0x928EFF)
         .bucketColor(0x928EFF)
-        .displayName('2级净化水')
+
 
     treeoflife.create('clear_water_tier_3')
         .thinTexture(0xA29EEF)
         .bucketColor(0xA29EEF)
-        .displayName('3级净化水')
+
 
     treeoflife.create('vibrant_fluix')
         .thickTexture(0x8A2BE2)
         .bucketColor(0x8A2BE2)
-        .displayName('谐振福鲁伊克斯')
+
         .temperature(400);
 
     treeoflife.create('stable_vibrant_fluix')
         .thickTexture(0x4B0082)
         .bucketColor(0x4B0082)
-        .displayName('稳定谐振福鲁伊克斯')
+
         .temperature(250);
 
     treeoflife.create('dragon_egg_fluid')
         .thickTexture(0x542054)
         .bucketColor(0x542054)
-        .displayName('龙蛋液')
+
         .temperature(300);
 
     treeoflife.create('magicless_dragon_egg_fluid')
         .thickTexture(0x340034)
         .bucketColor(0x340034)
-        .displayName('祛魔龙蛋液')
+
         .temperature(290);
 
     treeoflife.create('dragon_breath')
         .thickTexture(0xA470A4)
         .bucketColor(0xA470A4)
-        .displayName('龙息')
+
         .temperature(300);
 });
