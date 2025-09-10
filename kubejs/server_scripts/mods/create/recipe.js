56dc2994e25a3665387f2d6eea63fb2166c12e27
JRcrhd <116958216+JRcrhd@users.noreply.github.com>
2025-09-10 22:21:03 +0800

    update

--- kubejs/server_scripts/mods/create/recipe.js
+++ kubejs/server_scripts/mods/create/recipe.js
@@ -1,262 +1,290 @@
-ServerEvents.recipes(treeoflife => {
-    treeoflife.shaped('createdieselgenerators:burner',
-        [
-            'ABA',
-            ' C ',
-            'DED'
+ServerEvents.recipes((treeoflife) => {
+    treeoflife.shaped("createdieselgenerators:burner", ["ABA", " C ", "DED"], {
+        A: "forbidden_arcanus:obsidian_ingot",
+        B: "forbidden_arcanus:rune",
+        C: "create:shaft",
+        D: "create:andesite_alloy",
+        E: "create:empty_blaze_burner",
+    });
+
+    treeoflife.custom({
+        type: "createmetallurgy:melting",
+        heatRequirement: "superheated",
+        ingredients: [
+            {
+                tag: "forge:obsidian",
+            },
         ],
+        processingTime: 40,
+        results: [
+            {
+                amount: 810,
+                fluid: "kubejs:molten_obsidian",
+            },
+        ],
+    });
+
+    treeoflife.custom({
+        type: "createmetallurgy:melting",
+        heatRequirement: "heated",
+        ingredients: [
+            {
+                item: "enderio:dark_steel_ingot",
+            },
+        ],
+        processingTime: 40,
+        results: [
+            {
+                amount: 90,
+                fluid: "kubejs:molten_dark_steel",
+            },
+        ],
+    });
+
+    treeoflife.custom({
+        type: "createmetallurgy:alloying",
+        heatRequirement: "heated",
+        ingredients: [
+            {
+                amount: 90,
+                fluid: "kubejs:molten_obsidian",
+            },
+            {
+                amount: 10,
+                fluid: "createmetallurgy:molten_steel",
+            },
+        ],
+        processingTime: 40,
+        results: [
+            {
+                amount: 10,
+                fluid: "kubejs:molten_dark_steel",
+            },
+        ],
+    });
+
+    treeoflife.custom({
+        type: "createmetallurgy:casting_in_table",
+        ingredients: [
+            {
+                item: "createmetallurgy:graphite_ingot_mold",
+            },
+            {
+                amount: 90,
+                fluid: "kubejs:molten_dark_steel",
+            },
+        ],
+        processingTime: 60,
+        result: {
+            item: "enderio:dark_steel_ingot",
+        },
+    });
+
+    treeoflife.custom({
+        type: "createmetallurgy:casting_in_basin",
+        ingredients: [
+            {
+                amount: 810,
+                fluid: "kubejs:molten_dark_steel",
+            },
+        ],
+        processingTime: 320,
+        result: {
+            item: "enderio:dark_steel_block",
+        },
+    });
+
+    treeoflife.custom({
+        type: "createmetallurgy:casting_in_basin",
+        ingredients: [
+            {
+                amount: 810,
+                fluid: "kubejs:molten_obsidian",
+            },
+        ],
+        processingTime: 320,
+        result: {
+            item: "minecraft:obsidian",
+        },
+    });
+
+    treeoflife.custom({
+        type: "destroy:arc_furnace",
+        ingredients: [
+            {
+                item: "minecraft:charcoal",
+            },
+            {
+                tag: "forge:dusts/iron",
+            },
+            {
+                tag: "forge:dusts/iron",
+            },
+            {
+                tag: "forge:dusts/iron",
+            },
+        ],
+        results: [
+            {
+                item: "pneumaticcraft:ingot_iron_compressed",
+            },
+        ],
+        recipeDuration: 25600,
+    });
+
+    treeoflife.custom({
+        type: "custommachinery:custom_machine",
+        machine: "treeoflife:magic_vat",
+        time: 3600,
+        requirements: [
+            {
+                type: "custommachinery:fluid_per_tick",
+                fluid: "kubejs:witch_fluid",
+                amount: 2,
+                mode: "input",
+            },
+            {
+                type: "custommachinery:energy_per_tick",
+                amount: 2,
+                mode: "input",
+            },
+            {
+                type: "custommachinery:item",
+                item: "destroy:tear_bottle",
+                amount: 4,
+                mode: "input",
+            },
+            {
+                type: "custommachinery:item",
+                item: "minecraft:redstone",
+                amount: 64,
+                mode: "input",
+            },
+            {
+                type: "custommachinery:item",
+                item: "destroy:chromium_ingot",
+                amount: 2,
+                mode: "input",
+            },
+            {
+                type: "custommachinery:item",
+                item: "destroy:sodium_ingot",
+                amount: 6,
+                mode: "input",
+            },
+            {
+                type: "custommachinery:item",
+                item: "destroy:polyacrylonitrile",
+                amount: 1,
+                mode: "output",
+            },
+        ],
+    });
+
+    treeoflife.shaped(
+        Item.of("custommachinery:custom_machine_item").withNBT({
+            machine: "treeoflife:magic_vat",
+        }),
+        ["ABA", "CDC", "AEA"],
         {
-            A: 'forbidden_arcanus:obsidian_ingot',
-            B: 'forbidden_arcanus:rune',
-            C: 'create:shaft',
-            D: 'create:andesite_alloy',
-            E: 'create:empty_blaze_burner'
-        }
-    );
-
-    treeoflife.custom({
-        "type": "createmetallurgy:melting",
-        "heatRequirement": "superheated",
-        "ingredients": [
-            {
-                "tag": "forge:obsidian"
-            }
-        ],
-        "processingTime": 40,
-        "results": [
-            {
-                "amount": 810,
-                "fluid": "kubejs:molten_obsidian"
-            }
-        ]
-    });
-
-    treeoflife.custom({
-        "type": "createmetallurgy:melting",
-        "heatRequirement": "heated",
-        "ingredients": [
-            {
-                "item": "enderio:dark_steel_ingot"
-            }
-        ],
-        "processingTime": 40,
-        "results": [
-            {
-                "amount": 90,
-                "fluid": "kubejs:molten_dark_steel"
-            }
-        ]
-    });
-
-    treeoflife.custom({
-        "type": "createmetallurgy:alloying",
-        "heatRequirement": "heated",
-        "ingredients": [
-            {
-                "amount": 90,
-                "fluid": "kubejs:molten_obsidian"
-            },
-            {
-                "amount": 10,
-                "fluid": "createmetallurgy:molten_steel"
-            }
-        ],
-        "processingTime": 40,
-        "results": [
-            {
-                "amount": 10,
-                "fluid": "kubejs:molten_dark_steel"
-            }
-        ]
-    });
-
-    treeoflife.custom({
-        "type": "createmetallurgy:casting_in_table",
-        "ingredients": [
-            {
-                "item": "createmetallurgy:graphite_ingot_mold"
-            },
-            {
-                "amount": 90,
-                "fluid": "kubejs:molten_dark_steel"
-            }
-        ],
-        "processingTime": 60,
-        "result": {
-            "item": "enderio:dark_steel_ingot"
-        }
-    });
-
-    treeoflife.custom({
-        "type": "createmetallurgy:casting_in_basin",
-        "ingredients": [
-            {
-                "amount": 810,
-                "fluid": "kubejs:molten_dark_steel"
-            }
-        ],
-        "processingTime": 320,
-        "result": {
-            "item": "enderio:dark_steel_block"
-        }
-    });
-
-    treeoflife.custom({
-        "type": "createmetallurgy:casting_in_basin",
-        "ingredients": [
-            {
-                "amount": 810,
-                "fluid": "kubejs:molten_obsidian"
-            }
-        ],
-        "processingTime": 320,
-        "result": {
-            "item": "minecraft:obsidian"
-        }
-    });
-
-    treeoflife.custom({
-        "type": "destroy:arc_furnace",
-        "ingredients": [
-            {
-                "item": "minecraft:charcoal"
-            },
-            {
-                "tag": "forge:dusts/iron"
-            },
-            {
-                "tag": "forge:dusts/iron"
-            },
-            {
-                "tag": "forge:dusts/iron"
-            }
-        ],
-        "results": [
-            {
-                "item": "pneumaticcraft:ingot_iron_compressed"
-            }
-        ],
-        "recipeDuration": 25600
-    });
-    
-    treeoflife.custom({
-	    "type": "custommachinery:custom_machine",
-	    "machine": "treeoflife:magic_vat",
-	    "time": 3600,
-	    "requirements": [
-		    {
-			    "type": "custommachinery:fluid_per_tick",
-			    "fluid": "kubejs:witch_fluid",
-			    "amount": 2,
-			    "mode": "input"
-		    },
-		    {
-			    "type": "custommachinery:energy_per_tick",
-			    "amount": 2,
-			    "mode": "input"
-		    },
-            {
-			    "type": "custommachinery:item",
-			    "item": "destroy:tear_bottle",
-			    "amount": 4,
-			    "mode": "input"
-		    },
-            {
-			    "type": "custommachinery:item",
-			    "item": "minecraft:redstone",
-			    "amount": 64,
-			    "mode": "input"
-		    },
-            {
-			    "type": "custommachinery:item",
-			    "item": "destroy:chromium_ingot",
-			    "amount": 2,
-			    "mode": "input"
-		    },
-            {
-			    "type": "custommachinery:item",
-			    "item": "destroy:sodium_ingot",
-			    "amount": 6,
-			    "mode": "input"
-		    },
-		    {
-			    "type": "custommachinery:item",
-			    "item": "destroy:polyacrylonitrile",
-			    "amount": 1,
-			    "mode": "output"
-		    }
-	    ]
-    });
-
-    treeoflife.shaped(Item.of("custommachinery:custom_machine_item").withNBT({"machine":"treeoflife:magic_vat"}),
-        [
-            "ABA",
-            "CDC",
-            "AEA"
-        ],{
             A: "#forge:ingots/platinum",
             B: "destroy:dynamo",
             C: "destroy:touch_powder",
             D: "destroy:vat_controller",
-            E: "destroy:blacklight"
+            E: "destroy:blacklight",
         }
     );
 
     treeoflife.custom({
-        "type": "createmetallurgy:melting",
-        "heatRequirement": "superheated",
-        "ingredients": [
+        type: "createmetallurgy:melting",
+        heatRequirement: "superheated",
+        ingredients: [
             {
-                "tag": "forge:ingots/end_steel"
-            }
+                tag: "forge:ingots/end_steel",
+            },
         ],
-        "processingTime": 40,
-        "results": [
+        processingTime: 40,
+        results: [
             {
-                "amount": 90,
-                "fluid": "kubejs:molten_end_steel"
-            }
-        ]
+                amount: 90,
+                fluid: "kubejs:molten_end_steel",
+            },
+        ],
     });
 
     treeoflife.custom({
-        "type": "createmetallurgy:casting_in_table",
-        "ingredients": [
+        type: "createmetallurgy:casting_in_table",
+        ingredients: [
             {
-                "item": "createmetallurgy:graphite_ingot_mold"
+                item: "createmetallurgy:graphite_ingot_mold",
             },
             {
-                "amount": 90,
-                "fluid": "kubejs:molten_end_steel"
-            }
+                amount: 90,
+                fluid: "kubejs:molten_end_steel",
+            },
         ],
-        "processingTime": 60,
-        "result": {
-            "item": "enderio:end_steel_ingot"
-        }
+        processingTime: 60,
+        result: {
+            item: "enderio:end_steel_ingot",
+        },
     });
 
     treeoflife.custom({
-        "type": "createmetallurgy:melting",
-        "heatRequirement": "heated",
-        "ingredients": [
+        type: "createmetallurgy:melting",
+        heatRequirement: "heated",
+        ingredients: [
             {
-                "item": "aether:ambrosium_shard"
-            }
+                item: "aether:ambrosium_shard",
+            },
         ],
-        "processingTime": 40,
-        "results": [
+        processingTime: 40,
+        results: [
             {
-                "amount": 90,
-                "fluid": "kubejs:molten_ambrosium"
-            }
-        ]
+                amount: 90,
+                fluid: "kubejs:molten_ambrosium",
+            },
+        ],
     });
-})
+
+    treeoflife.custom({
+        type: "create:mixing",
+        ingredients: [
+            {
+                item: "pneumaticcraft:glycerol",
+                amount: 1,
+            },
+        ],
+        results: [
+            {
+                fluid: "destroy:mixture",
+                amount: 500,
+                nbt: {
+                    Mixture: {
+                        FluidName: "destroy:glycerol",
+                        Contents: [
+                            {
+                                Molecule: "destroy:glycerol",
+                                Concentration: 10.858471,
+                            },
+                        ],
+                    },
+                },
+            },
+        ],
+    });
+});
 
 LootJS.modifiers((treeoflife) => {
-    treeoflife.addLootTypeModifier(LootType.CHEST).replaceLoot("create:sturdy_sheet", "destroy:nickel_ingot");
-    treeoflife.addLootTypeModifier(LootType.CHEST).replaceLoot("create:brass_hand", "destroy:lead_ingot");
-    treeoflife.addLootTypeModifier(LootType.CHEST).replaceLoot("create:brass_ingot", "destroy:lead_ingot");
-    treeoflife.addLootTypeModifier(LootType.CHEST).replaceLoot("create:brass_nugget", "destroy:nickel_nugget");
+    treeoflife
+        .addLootTypeModifier(LootType.CHEST)
+        .replaceLoot("create:sturdy_sheet", "destroy:nickel_ingot");
+    treeoflife
+        .addLootTypeModifier(LootType.CHEST)
+        .replaceLoot("create:brass_hand", "destroy:lead_ingot");
+    treeoflife
+        .addLootTypeModifier(LootType.CHEST)
+        .replaceLoot("create:brass_ingot", "destroy:lead_ingot");
+    treeoflife
+        .addLootTypeModifier(LootType.CHEST)
+        .replaceLoot("create:brass_nugget", "destroy:nickel_nugget");
 });
--- kubejs/server_scripts/mods/projecte/recipes.js
+++ kubejs/server_scripts/mods/projecte/recipes.js
@@ -12,4 +12,28 @@ ServerEvents.recipes(treeoflife => {
 	treeoflife.remove({id: "projecte:relay_mk1"});
 	treeoflife.remove({id: "projecte:relay_mk2"});
 	treeoflife.remove({id: "projecte:relay_mk3"});
+
+	treeoflife.shaped("projecte:low_covalence_dust", [" A ", "CBC", " A "], {
+		A: "cataclysm:void_stone",
+		B: "cataclysm:essence_of_the_storm",
+		C: "#forge:gems/zanite"
+	});
+
+	treeoflife.shaped("projecte:medium_covalence_dust", [" A ", "CBC", " A "], {
+		A: "art_of_forging:eerie_shard",
+		B: "projecte:low_covalence_dust",
+		C: "cataclysm:crystallized_coral_fragments"
+	});
+
+	treeoflife.shaped("projecte:high_covalence_dust", ["DAD", "CBC", "DAD"], {
+		A: "goety_cataclysm:abyss_essence",
+		B: "projecte:medium_covalence_dust",
+		C: "destroy:fluorite",
+		D: "aether:swet_ball"
+	});
+
+    treeoflife.smithing('projecte:alchemical_coal', 'projecte:low_covalence_dust', 'minecraft:coal', 'destroy:dynamite');
+    treeoflife.smithing('projecte:mobius_fuel', 'projecte:medium_covalence_dust', 'projecte:alchemical_coal', 'create_enchantment_industry:hyper_experience_bottle');
+    treeoflife.smithing('projecte:aeternalis_fuel', 'projecte:high_covalence_dust', 'projecte:mobius_fuel', 'rainbowcompound:netherstar_ingot');
+
 })
